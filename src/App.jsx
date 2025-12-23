837094ba3412048cc913af9c9e62b80202617302
argyris3 <78222306+argyris3@users.noreply.github.com>
2025-12-23 11:11:20 +0200

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
-gsap.registerPlugin(ScrollTrigger)
+import NavBar from './sections/Navbar';
+import Hero from './sections/Hero';
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-      <div>
-        <h1 className='text-3xl text-red-500'>gta motherfuckers</h1>
-      </div>
+      <NavBar />
+      <Hero />
     </main>
   );
 };
